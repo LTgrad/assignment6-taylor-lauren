9fcd9364036b86b7022baddb21edf939fa876639
Lauren Taylor <lauren.taylor@ufl.edu>
2019-04-28 16:53:24 -0400

    added new javascript

---
+++ assignment6-taylor,lauren/assignment6/scripts/main.js
@@ -0,0 +1,26 @@
+//array
+
+var members = [
+  'Dave Grohl',
+  'Nate Mendel',
+  'Pat Smear',
+  'Taylor Hawkins',
+  'Rami Jaffee',
+  'Chris Shiflett'
+];
+//object
+
+var tour = {
+Chicago: 'July 29th & 30th, 2018',
+Seattle: 'September 1st, 2018',
+Edmonton: 'September 4th, 2018'
+};
+
+//while loop
+var x = 0;
+
+while (x < 5) {
+  console.log('Times running through the loop' + x);
+
+  x++;
+};
